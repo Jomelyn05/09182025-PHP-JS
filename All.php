4b55c1370e945e072112f8e88955cda724f1e3be
senku-1 <ojtdevai@comfac-it.com>
2025-09-18 15:02:17 +0800

    create php

---
+++ All.php
@@ -0,0 +1,148 @@
+<?php 
+        $name = 'Zura';
+        $surname = 'Liza';
+        $age = '32';
+        $email = 'Zura@gmail.com';;
+        $title = 'PHP';
+
+        // Print variables with echo.
+        echo "<hr> $title";
+        echo "<br> <hr> $name  " . $surname ;
+        echo "<br> $age <br>  $email" ."<br>";
+        echo "<hr>";
+
+        //string part!
+        //strtolower - make all the sentence
+        $title = 'Php String';
+         echo $title . '<br> <hr>';
+    
+        $str = "String Php";
+
+        echo strtolower($str) .'<br>';
+
+        echo strtoupper($str) .'<br>';
+
+        echo str_replace("Php","Secret!", $str) .'<br>';
+
+        echo str_word_count($str) .'<br>';
+
+        echo "<hr>";
+
+        //number!
+
+        $first = "Number";
+        echo "$first <br> <hr>";
+
+        $x = "5";
+        $y = "5";
+
+        echo $x - $y;
+        echo "<br>";
+        echo $x + $y;
+        echo "<br>";
+        echo $x * $y;
+        echo "<br>";
+        echo $x / $y;
+        echo "<br> <hr>";
+
+        //Conditional!
+
+        $head = "Conditional";
+        echo $head ."<br> <hr>";
+
+        //if-else
+        $age = 10;
+
+        if ($age <= 12) { 
+            
+            echo "TOO YOUNG TO WATCH HARRY POTTER" . '<br>';
+
+            } else if ($age > 15) { 
+                
+                echo "YOU MAY WATCH HARRY POTTER" . '<br>';
+
+            } else { 
+                
+                echo "YOU SHOULD WATCH HARRY POTTER" . '<br>';
+
+            }
+
+             $age = 20;
+
+        if ($age <= 12) { 
+            
+            echo "<br> TOO YOUNG TO WATCH HARRY POTTER" . '<br> <hr>';
+
+            } else if ($age > 15) { 
+                
+                echo "<br> YOU MAY WATCH HARRY POTTER" . '<br> <hr>';
+
+            } else { 
+                
+                echo "YOU SHOULD WATCH HARRY POTTER <br> <hr>";
+
+            }
+
+            //logical Operators
+            // && - and , || - or, ! - not
+            $namess = 'Logical Operators - Conditional';
+            echo $namess . '<br>';
+            $age = 20;
+            $watched = "true";
+
+        if ($age <= 15 && !$watched) { 
+            
+            echo "<br> YOU MUST WATCH HARRY POTTER";
+
+            }
+
+        if ($age >= 15 || !$watched) {
+            echo "<br> YOU MUST NOT WATCH HARRY POTTER <hr>";
+        }
+
+        //SWITCH!!
+        $names = 'Switch - Conditional';
+        echo $names .'<br>';
+
+        $day = 5;
+
+        switch ($day) {
+            case 1:
+                
+                echo "<br> MONDAY" . '<hr>';
+                break;
+
+            case 2:
+
+                echo "<br> TUESDAY" . '<hr>';
+                break;
+
+            case 3: 
+                
+                echo "<br> WEDNESDAY" . '<hr>';
+                break;
+            
+            case 4:
+
+                echo "<br> THURSDAY" . '<hr>';
+                break;
+            
+            case 5: 
+                echo "<br> FRIDAY" . '<hr>';
+                break;
+        }
+
+        //ARRAYS!!
+
+        $aray = 'Arrays - PHP';
+        echo $aray .'<br> <hr>';
+
+        // INDEXED ARRAY
+         $array = 'Indexed - Arrays';
+        echo $array .'<br> <hr>';
+
+        $fruits = ["Apple", "Avocado", "Star Fruits"];
+        echo $fruits[2] .'<br>';
+        $fruits[3] = 'Mango';
+        var_dump($fruits);
+?>
