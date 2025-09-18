9fe5fcb67e780f61725e4b880b08278475b11b1d
senku-1 <ojtdevai@comfac-it.com>
2025-09-18 16:01:16 +0800

    create number php

---
+++ number.php
@@ -0,0 +1,20 @@
+<?php
+
+        //number!
+
+        $first = "Number";
+            echo "$first <br> <hr>";
+
+        $x = "5";
+        $y = "5";
+
+            echo $x - $y;
+            echo "<br>";
+            echo $x + $y;
+            echo "<br>";
+            echo $x * $y;
+            echo "<br>";
+            echo $x / $y;
+            echo "<br> <hr>";
+            
+?>
